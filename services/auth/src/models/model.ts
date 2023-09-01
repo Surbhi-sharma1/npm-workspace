e9ddee6ad648ec52b77f2d8007cdf9dc9b9da886
Surbhi  Sharma <surbhi.sharma1@sourcefuse.com>
2023-09-01 15:48:56 +0530

    feat(ci-cd): add model in services
    add model in services
    
    GH-0

---
+++ services/auth/src/models/model.ts
@@ -0,0 +1,44 @@
+import {property} from '@loopback/repository';
+
+export class User {
+  @property({
+    type: 'string',
+    id: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+    name: 'first_name',
+  })
+  firstName: string;
+
+  @property({
+    type: 'string',
+    name: 'last_name',
+  })
+  lastName: string;
+
+  @property({
+    type: 'string',
+    name: 'middle_name',
+  })
+  middleName?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  username: string;
+
+  @property({
+    type: 'string',
+  })
+  email?: string;
+  @property({
+    type: 'string',
+    name: 'auth_client_ids',
+  })
+  authClientIds?: string;
+}
