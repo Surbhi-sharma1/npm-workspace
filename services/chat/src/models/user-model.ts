df7792510fb7f007c516a87abb23c16430193c0e
Surbhi  Sharma <surbhi.sharma1@sourcefuse.com>
2023-09-04 15:29:22 +0530

    feat(services): add user model in services
    add user model in services
    
    GH-0

---
+++ services/chat/src/models/user-model.ts
@@ -0,0 +1,34 @@
+import {property} from '@loopback/repository';
+
+export class User {
+  @property({
+    type: 'string',
+    id: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+    name: 'first_name',
+  })
+  firstName: string;
+
+  @property({
+    type: 'string',
+    name: 'last_name',
+  })
+  lastName: string;
+
+  @property({
+    type: 'string',
+    name: 'middle_name',
+  })
+  middleName?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  username: string;
+}
